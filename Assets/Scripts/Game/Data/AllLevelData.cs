3a5b0bfb8088858544a08697cd3530847dbef33f
BlaBlaArt <ivanovartemw@gmail.com>
2021-03-18 13:27:30 +0200

    naughty attributes

--- Assets/Scripts/Game/Data/AllLevelData.cs
+++ Assets/Scripts/Game/Data/AllLevelData.cs
@@ -1,31 +1,37 @@
+using NaughtyAttributes;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "All level Data", fileName = "New All level Data", order = 1)]
 public class AllLevelData : ScriptableObject
 {
-    [Header("AssembleStage")]
+    [BoxGroup("Assemble Stage")]
     public float HeightOFBoxOffset;
+    [BoxGroup("Assemble Stage")]
     public float DeltaTimeToMoveBox;
     
 
-    [Header("CellStage")]
+    [BoxGroup("Cell Stage")]
     public float OffsetToSpawnCharacters;
-
+    [BoxGroup("Cell Stage")]
     public Color StartCellColor, ActiveCellColor;
 
     
-    [Header("DragStage")]
+    [BoxGroup("Drag Stage")]
     public float DeltaHeightOfLevel;
+    [BoxGroup("Drag Stage")]
     public float OffsetToForward;
+    [BoxGroup("Drag Stage")]
     public float FollowSmooth;
+    [BoxGroup("Drag Stage")]
     public LayerMask PlaneForCamera;
+    [BoxGroup("Drag Stage")]
     public float CellsHeight;
     
     
-    [Header("FightStageHero")]
+    [BoxGroup("Fight Stage Hero")] [ShowAssetPreview(128,128)]
     public GameObject ArcherPref, WariorPref, WizardPref, ShieldPref;
     
-    [Header("FightStageEnemy")]
+    [BoxGroup("Fight Stage Enemies")] [ShowAssetPreview(128,128)]
     public GameObject ArcherEnemyPref, WariorEnemyPref, WizardEnemyPref, ShieldEnemyPref;
 
 
